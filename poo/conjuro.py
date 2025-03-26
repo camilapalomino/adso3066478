9cfaa97da1c32e3913cd1ae6895e9973170f2b73
camilapalomino <palominocamila17@gmail.com>
2025-03-25 20:01:31 -0500

    Delete poo directory

--- poo/conjuro.py
+++
@@ -1,5 +0,0 @@
-class Conjuro:
-    def __init__(self, nombre, poder):
-    self.nombre = nombre
-    self.poder = poder
-    
--- poo/espada.py
+++
@@ -1,4 +0,0 @@
-class Espada:
-    def __init__(self, nombre, danio):
-        self.nombre = nombre
-        self.danio = danio
